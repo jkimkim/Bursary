25ee2a661ab0a9eae44380284493305084c55aa0
johnwairia <wairia21@gmail.com>
2022-10-25 19:07:42 +0300

    add user class

--- app/src/main/java/com/example/bursary/User.java
+++ app/src/main/java/com/example/bursary/User.java
@@ -1,2 +1,55 @@
 package com.example.bursary;public class User {
+    private String name;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String date;
+
+    public User(String name, String email, String password, String confirmPassword, String date) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
