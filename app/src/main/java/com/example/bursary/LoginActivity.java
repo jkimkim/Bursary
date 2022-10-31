993cfb4b695fa5cfe8e402b6240e1a005c5175a9
johnwairia <wairia21@gmail.com>
2022-10-31 17:14:47 +0300

    update

--- app/src/main/java/com/example/bursary/LoginActivity.java
+++ app/src/main/java/com/example/bursary/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.example.bursary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView register;
+    private EditText editTextEmail, editTextPassword;
 
+    private  Button login;
+
+    private FirebaseAuth mAuth;
+
+    ProgressDialog progressDialog;
 
 
     @Override
@@ -17,11 +35,75 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        login = (Button) findViewById(R.id.buttonLogin) ;
+        login.setOnClickListener(this);
+
+
+        editTextEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        editTextPassword = (EditText) findViewById(R.id.editTextNumberPassword);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+
         register = findViewById(R.id.register);
         register.setOnClickListener(v -> {
             startActivity(new Intent(LoginActivity.this, SignupActivity.class));
         });
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonLogin:
+                userLogin();
+        }
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please provide valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Password must be at least 6 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressDialog.show();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    //redirect to main activity
+
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Filed to log in! Please check your credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
