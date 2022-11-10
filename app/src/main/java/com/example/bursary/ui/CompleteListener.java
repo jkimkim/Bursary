6642ab7c1098f3fda362acf46bdb1932efdf3610
johnwairia <wairia21@gmail.com>
2022-11-10 21:26:58 +0300

    update

---
+++ app/src/main/java/com/example/bursary/ui/CompleteListener.java
@@ -0,0 +1,20 @@
+package com.example.bursary.ui;
+
+import com.example.bursary.Upload;
+
+import java.util.List;
+
+public interface CompleteListener {
+    default void onComplete(String downLoadUrl){
+
+    }
+
+    default void onUploadFetched(List<Upload> uploads){
+
+    }
+
+    default void onGenderGroup(int maleCount, int femaleCount,int others){
+
+    }
+}
+
---
+++ app/src/main/java/com/example/bursary/ui/UploadUtils.java
@@ -0,0 +1,5 @@
+package com.example.bursary.ui;
+
+public class UploadUtils {
+    String downloadUrl;
+}
