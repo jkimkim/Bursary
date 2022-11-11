9e603eb9d732176ba9c041dfae9a29c4591610fa
johnwairia <wairia21@gmail.com>
2022-11-11 15:14:08 +0300

    update

--- app/src/main/java/com/example/bursary/SignupActivity.java
+++ app/src/main/java/com/example/bursary/SignupActivity.java
@@ -3,12 +3,14 @@ package com.example.bursary;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,7 +24,10 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener , DatePickerDialog.OnDateSetListener{
 
     private FirebaseAuth mAuth;
 
@@ -49,6 +54,8 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         txtPassword = findViewById(R.id.txtPassword);
         txtConfirmPassword = findViewById(R.id.txtConfirmPassword);
         editTextDate = findViewById(R.id.editTextDate);
+        editTextDate.setOnClickListener(this);
+
         btnRegister = findViewById(R.id.btnRegister);
 
 
@@ -69,8 +76,13 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.btnRegister:
                 registerUser();
                 break;
+            case R.id.editTextDate:
+                com.example.bursary.ui.DatePicker mDatePickerDialogFragment = new com.example.bursary.ui.DatePicker();
+                mDatePickerDialogFragment.show(getSupportFragmentManager(), "Date Picker");
+                break;
         }
     }
+
 
     private void registerUser() {
         String name = txtName.getText().toString().trim();
@@ -171,5 +183,15 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         //register the user in firebase
 
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar mCalendar = Calendar.getInstance();
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+        editTextDate.setText(selectedDate);
     }
 }
--- app/src/main/java/com/example/bursary/ui/DatePickerDialog.java
+++
@@ -1,4 +0,0 @@
-package com.example.bursary.ui;
-
-public class DatePickerDialog {
-}
