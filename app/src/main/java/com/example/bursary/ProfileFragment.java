8a3f652feac5ec9b6a4fa74cb7d0e7acccb8316d
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-01-15 19:42:05 +0300

    add profile

--- app/src/main/java/com/example/bursary/ProfileFragment.java
+++ app/src/main/java/com/example/bursary/ProfileFragment.java
@@ -7,32 +7,61 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bursary.databinding.FragmentProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileFragment extends Fragment {
 
-        /**
-        * Use this factory method to create a new instance of
-        * this fragment using the provided parameters.
-        *
-        * @return A new instance of fragment ProfileFragment.
-        */
+    private  TextView fullName,email,phoneNo;
+    private String name,emailId,phone;
+    private ImageView profileImage;
+    FirebaseAuth fAuth;
 
         private FragmentProfileBinding binding;
 
-        public View onCreateView(@NonNull LayoutInflater inflater,
-                                    ViewGroup container, Bundle savedInstanceState) {
-                binding = FragmentProfileBinding.inflate(inflater, container, false);
-                View root = binding.getRoot();
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
-                return root;
-            }
+        fullName = view.findViewById(R.id.full_name);
+        email = view.findViewById(R.id.email);
+        phoneNo = view.findViewById(R.id.phone);
+        profileImage = view.findViewById(R.id.profile_image);
+        fAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = fAuth.getCurrentUser();
 
-            @Override
-            public void onDestroyView() {
-                super.onDestroyView();
-                binding = null;
+        if (user != null){
+            showUserData(user);
+        }
+
+        return view;
+    }
+
+    private void showUserData(FirebaseUser user) {
+        //extracting data from firebase database
+        String uId = user.getUid();
+
+        //extracting data from firebase database
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(uId).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                name = task.getResult().child("name").getValue(String.class);
+                emailId = task.getResult().child("email").getValue(String.class);
+                phone = task.getResult().child("phone").getValue(String.class);
+
+                fullName.setText(name);
+                email.setText(emailId);
+                phoneNo.setText(phone);
             }
+        });
+    }
 }
